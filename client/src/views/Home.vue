ee720ea3e4c5d93f8c0d450a4ef8a618f8d93d82
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2020-11-27 18:19:50 +0100

    intial frontend application files

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <navigation-bar></navigation-bar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavigationBar from '../components/ui/NavigationBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    NavigationBar
+  }
+}
+</script>
