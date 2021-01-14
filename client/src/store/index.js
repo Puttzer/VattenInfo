2732d6331216ba8bee27f0e27e9a1044090582f4
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-01-14 08:27:54 +0100

    introducing modules

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import user from './modules/user';
+import company from './modules/comapny'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    user,
+    company
   }
 })
---
+++ client/src/store/modules/comapny.js
@@ -0,0 +1,17 @@
+// import Vue from 'vue'
+
+//declaring the state variables
+const state = {}
+//applying getters to the state 
+const getters = {}
+//synchronous task using mutations
+const mutations = {}
+//asynchronous task using actions
+const actions = {}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
