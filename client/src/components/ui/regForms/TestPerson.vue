5c56c148e05c32f75fee9cd1d5a182b6c9a706f0
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-01-30 23:56:03 +0100

    register and login pages

--- client/src/components/ui/regForms/TestPerson.vue
+++ client/src/components/ui/regForms/TestPerson.vue
@@ -1,40 +1,52 @@
 <template>
   <v-container>
-    <form action>
+    <form class="mt-n12 pa-10" >
+      <v-row d-flex justify="center">
+        <v-col cols="12" md="12">
+          <h3>Skappa Privat Person Konto</h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="12">
+          <v-text-field dense color="#051f38" text class="mt-6" v-model="privat.name" append-icon="person" label="Namn" outlined placeholder="krishna Mohan" ></v-text-field>
+        </v-col>
+      </v-row>
       <v-row justify="center">
         <v-col cols="12" md="12">
-          <v-text-field v-model="privat.name" label="namn" outlined></v-text-field>
+          <v-text-field color="#051f38" text dense class="mt-n3"  append-icon="email" v-model="privat.email" label="Email" outlined placeholder="testemail@test.com"></v-text-field>
         </v-col>
       </v-row>
 
       <v-row justify="center">
-        <v-col cols="12" md="12">
-          <v-text-field v-model="privat.email" label="email" outlined></v-text-field>
+        <v-col cols="12" md="6">
+          <v-text-field dense class="mt-n3" color="#051f38" append-icon="phone" text v-model="privat.phone" label="Phone" outlined placeholder="0855555555"></v-text-field>
+        </v-col>
+      
+        <v-col cols="12" md="6">
+          <v-text-field dense class="mt-n3" append-icon="contact_phone" color="#051f38" text v-model="privat.altPhone" label="Alternativt Telefonnummer" outlined placeholder="0855555555"></v-text-field>
         </v-col>
       </v-row>
 
       <v-row justify="center">
-        <v-col cols="12" md="12">
-          <v-text-field v-model="privat.phone" label="phone" outlined></v-text-field>
+        <v-col cols="12" md="9">
+          <v-text-field dense color="#051f38" append-icon="place" text v-model="privat.adress" label="Street Name" outlined placeholder="odalvägen"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field dense color="#051f38" text v-model="privat.adress" label="Number" outlined placeholder="23"></v-text-field>
         </v-col>
       </v-row>
 
       <v-row justify="center">
-        <v-col cols="12" md="12">
-          <v-text-field v-model="privat.altPhone" label="alternativt telefonnummer" outlined></v-text-field>
+        <v-col cols="12" md="6">
+          <v-text-field dense class="mt-n3" append-icon="location_city" color="#051f38" text v-model="privat.city" label="Postort" outlined placeholder="Linköping"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field dense class="mt-n3" color="#051f38"  text v-model="privat.postnr" label="Post nummer" outlined placeholder="19535"></v-text-field>
         </v-col>
       </v-row>
-
       <v-row justify="center">
-        <v-col cols="12" md="12">
-          <v-text-field v-model="privat.adress" label="adress" outlined></v-text-field>
-        </v-col>
-      </v-row>
-
-      <v-row justify="center">
-        <v-col cols="6" md="6">
-          <v-text-field v-model="privat.city" label="postort" outlined></v-text-field>
-          <v-text-field v-model="privat.postnr" label="post nummer" outlined></v-text-field>
+        <v-col cols="12" md="3">
+          <v-btn> Register </v-btn>
         </v-col>
       </v-row>
 
@@ -60,5 +72,5 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 </style>
