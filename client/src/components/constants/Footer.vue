6425d71a831ab3588555952208b2748099067085
Patrick Carragher <carragher@live.se>
2021-02-01 15:18:29 +0100

    icon fitted

--- client/src/components/constants/Footer.vue
+++ client/src/components/constants/Footer.vue
@@ -2,17 +2,21 @@
   <v-container>
     <v-row d-flex flex-row>
       <v-col
-        ><!-- <v-img></v-img> -->
-        <p>LOGO</p>
-        <p>telephone</p>
+        ><v-img
+          max-height="60px"
+          max-width="63px"
+          src="https://i.imgur.com/DXOCecV.png"
+        ></v-img>
+
         <p>
           Kaserngatan 11 <br />
           76146 Norrtälje
         </p>
+        <p>08-428 431 50</p>
       </v-col>
       <v-col d-flex class="d-flex align-end mb-6">copyright</v-col>
       <v-col></v-col>
-      <v-col d-flex class="d-flex align-end mb-6">SOCIALS</v-col>
+      <v-col d-flex class="d-flex align-end mb-6">SOCIALS ICONS here</v-col>
     </v-row>
   </v-container>
 </template>
