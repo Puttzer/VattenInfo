6acde1df6d4fd528e51524d381cab7dca993098e
kaoskar1 <oscar.ljungdahl@hotmail.com>
2021-03-18 10:27:18 +0100

    hata aik

--- server/Middleware/Image.upload.js
+++ server/Middleware/Image.upload.js
@@ -2,7 +2,7 @@
 const multer = require('multer')
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) { cb(null, './uploads/'); },
-	filename: function (req, file, cb) { cb(null, new Date().toISOString()  + file.originalname); }
+	filename: function (req, file, cb) { cb(null,file.originalname); }
 });
 const fileFilter = (req, file, cb) => {   // reject a file   
 	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') { cb(null, true); } else { cb(null, false); }
