dd61e534e59f7ccba85ee9a1b6a610c8eb2a9a71
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-03-24 17:00:12 +0100

    delete company user

--- client/src/store/modules/company.js
+++ client/src/store/modules/company.js
@@ -1,6 +1,8 @@
 export default {
     state: {
         companys: [],
+        errorMessage: '',
+        sucessMessage: '',
         companyUserIsloggedIn: false,
         companyUser: {
             email: '',
@@ -24,54 +26,59 @@ export default {
 
             const data = await response.json()
             console.log(data.companys)
+            commit('UPDATE_SUCESS_MESSAGE', data.message, { module: 'company' })
             commit('UPDATE_COMPANYS', data.companys, { module: 'company' })
 
         },
-//         // async deleteUser({ commit }, id) {
-//         //     console.log('move to dispatch')
-//         //     const token = localStorage.getItem('token')
-//         //     const response = await fetch(`http://localhost:4000/api/user/${id}`, {
-//         //         method: 'DELETE',
-//         //         headers: {
-//         //             'Content-Type': 'application/json',
-//         //             'authorization': token,
-//         //         }
-//         //     })
+        async deleteCompany({ commit }, id) {
+            console.log('move to dispatch')
+            const token = localStorage.getItem('token')
+            const response = await fetch(`http://localhost:4000/api/company/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': token,
+                }
+            })
 
-//             const data = await response.json()
-//             console.log(data)
-//             commit('DELETE_USER', data.user._id, { module: 'user' })
+            const data = await response.json()
+            console.log(data)
+            commit('DELETE_COMPANY', data.company._id, { module: 'company' })
 
-//         },
-//         async loginUser({ commit }, payload) {
-//             console.log(payload)
+        },
+        //         async loginUser({ commit }, payload) {
+        //             console.log(payload)
 
-//             const response = await fetch(`http://localhost:4000/api/user/login`, {
-//                 method: 'POST',
-//                 body: JSON.stringify(payload),
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 }
-//             })
+        //             const response = await fetch(`http://localhost:4000/api/user/login`, {
+        //                 method: 'POST',
+        //                 body: JSON.stringify(payload),
+        //                 headers: {
+        //                     'Content-Type': 'application/json'
+        //                 }
+        //             })
 
-//             const data = await response.json()
-//             console.log(data)
-//             localStorage.setItem('userToken', data.Token)
-//             commit('UPDATE_USER_EMAIL', data.email, { module: 'user' })
-//             commit('UPDATE_USER_ID', data._id, { module: 'user' })
-//             commit('UPDATE_USER_ISLOGGEDIN', true, { module: 'user' })
+        //             const data = await response.json()
+        //             console.log(data)
+        //             localStorage.setItem('userToken', data.Token)
+        //             commit('UPDATE_USER_EMAIL', data.email, { module: 'user' })
+        //             commit('UPDATE_USER_ID', data._id, { module: 'user' })
+        //             commit('UPDATE_USER_ISLOGGEDIN', true, { module: 'user' })
 
-//         }
+        //         }
     },
     mutations: {
         UPDATE_COMPANYS(state, value) {
             state.companys = value
         },
-        // DELETE_USER(state, _id) {
-        //     const remainingUsers = state.users.filter(user => user._id !== _id)
-        //     console.log(remainingUsers)
-        //     state.users = remainingUsers
-        // },
+        DELETE_COMPANY(state, _id) {
+            const remainingUsers = state.companys.filter(company => company._id !== _id)
+            console.log(remainingUsers)
+            state.companys = remainingUsers
+        },
+        UPDATE_SUCESS_MESSAGE(state, value) {
+            state.sucessMessage = value
+            // this.$vToastify.success(state.sucessMessage);
+        },
         // UPDATE_USER_EMAIL(state, email) {
         //     state.user.email = email
         // },
