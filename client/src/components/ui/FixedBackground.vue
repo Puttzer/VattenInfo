964e1b0c8a645ffe0d90018365210db7914c9f79
Patrick Carragher <carragher@live.se>
2021-03-25 13:00:50 +0100

    separated content and visual assets

---
+++ client/src/components/ui/FixedBackground.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+        height="200px"
+        cols="12"
+        md="2"
+        class="d-flex align-center justify-start"
+      >
+        <div class="wave">
+          <v-img
+            src="@/assets/landing/waves.png"
+            max-height="100"
+            max-width="100"
+            border-radius="100"
+          ></v-img>
+        </div>
+      </v-col>
+      <v-col cols="12" md="7" class="d-flex justify-center">
+        <div class="middle_section">
+          <div class="labb blue--text ma-2">VATTENINFO LABBTJÄNSTER</div>
+          <div class="rubrik ma-0 pa-0">
+            <p class="green--text ma-0 pa-0">kvalitetssäkra</p>
+            <p class="blue--text mt-0 pt-0">ditt vatten.</p>
+          </div>
+          <div class="bread">
+            <p class="body-2">
+              Vi erbjuder mikrobiologiska och kemiska analyser för enskilt
+              vatten, livsmedelsklassat vatten samt avlopp
+            </p>
+          </div>
+          <v-btn class="btnColor white--text">läs mer</v-btn>
+        </div>
+      </v-col>
+
+      <v-col cols="12" md="3" class="d-flex flex-row align-start">
+        <svg viewBox="0 0 500 500">
+          <path
+            id="curve"
+            fill="transparent"
+            d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97"
+          />
+          <text width="500">
+            <textPath xlink:href="#curve">bli mer vattenmedveten</textPath>
+          </text>
+        </svg>
+        <v-img
+          src="@/assets/landing/OmtankeH20.png"
+          md="6"
+          contain
+          max-height="500"
+          max-width="500"
+        ></v-img>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ client/src/views/ScrollHome.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="background container mt-6">
+    <FixedBackground />
+
+    <!-- landning fort -->
+
+    <v-row>
+      <v-col cols="5" class>
+        <v-row class="d-flex justify-center align-center">
+          <h1 class="blue--text font-weight-class text-uppercase text-center">
+            Ta hand om vattnet
+            <br />så tar vattnet hand om dig
+          </h1>
+          <p class="text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+            doloremque voluptatem dignissimos soluta incidunt non velit
+            recusandae quia amet quibusdam.
+          </p>
+        </v-row>
+        <v-row class="d-flex justify-space-between">
+          <v-col class cols="3">
+            <div class="provVal d-flex flex-column justify-start align-center">
+              <v-icon size="84">mdi-bottle-tonic-outline</v-icon>
+              <div class="provInfo d-flex flex-column pt-6 align-center blue">
+                <h5>name</h5>
+                <p class="caption-1">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing.
+                </p>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="3">
+            <div class="provVal d-flex flex-column justify-start align-center">
+              <v-icon size="84">mdi-bottle-tonic-outline</v-icon>
+              <div class="provInfo d-flex flex-column pt-6 align-center blue">
+                <h5>name</h5>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing.</p>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="3">
+            <div class="provVal d-flex flex-column justify-start align-center">
+              <v-icon size="84">mdi-bottle-tonic-outline</v-icon>
+              <div class="provInfo d-flex flex-column pt-6 align-center blue">
+                <h5>name</h5>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing.</p>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-btn color="blue" class="white--text">FLER PROVER</v-btn>
+        </v-row>
+      </v-col>
+      <v-col cols="5" class=""> </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import FixedBackground from "../components/ui/FixedBackground";
+export default {
+  components: {
+    FixedBackground,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+.labb {
+  letter-spacing: 5px;
+  font-family: "Poppins", sans-serif;
+  font-weight: lighter;
+  font-size: 14px;
+}
+
+/*  text starts here */
+.rubrik {
+  text-transform: uppercase;
+  font-size: 42px;
+  /* font-family: 'Poppins', sans-serif; */
+  font-family: "Roboto", sans-serif;
+  font-weight: bold;
+}
+
+.bread {
+  /* letter-spacing: 3px; */
+  font-family: "Poppins", sans-serif;
+  font-weight: lighter;
+  font-size: 9px;
+  width: 300px;
+}
+
+/* text  ends here here */
+
+/* curved text starts here */
+
+/* .logo {
+  
+  background: url("../assets/landing/OmtankeH20.png");
+} */
+path {
+  fill: transparent;
+}
+
+text {
+  font-family: "Poppins", sans-serif;
+  font-size: 14px;
+  text-transform: uppercase;
+  fill: #449a63;
+}
+
+/* curved text ends here */
+
+/* landingfort starts here */
+.heading-fort {
+  text-transform: uppercase;
+  font-size: 28px;
+  font-weight: 800;
+}
+
+.bread-fort {
+  /* width: 300px; */
+  /* text-align: center; */
+  font-weight: 300;
+  /* width: 200px; */
+}
+
+/* .provInfo {
+  width: 200px;
+  height: 150px;
+  text-align: center;
+  color: white;
+} */
+
+/* landingfort ends here */
+</style>
