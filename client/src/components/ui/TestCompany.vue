fbce0bde0fd478516b8bdac5b0898903dd035804
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-05-17 12:36:20 +0200

    putting the files back

---
+++ client/src/components/ui/TestCompany.vue
@@ -0,0 +1,294 @@
+<template>
+  <v-row>
+    <v-col>
+      <form class="pa-8">
+        <v-row d-flex justify="center">
+          <v-col cols="12" md="12" justify="center">
+            <h3>Skappa Ett Företag Konto</h3>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              placeholder="Företagsnamn"
+              v-model="company.name"
+              label="Företags Namn"
+              outlined
+              append-icon="work"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="Förnamn Efternamn"
+              v-model="company.contactPerson"
+              label="Kontakt Person"
+              outlined
+              append-icon="person"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="test@test.com"
+              v-model="company.contactEmail"
+              label="Email"
+              outlined
+              append-icon="email"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row justify="center">
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              password
+              color="#051f38"
+              placeholder="password"
+              v-model="company.password"
+              label="password"
+              outlined
+              append-icon="lock"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row justify="center">
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              :type="password"
+              color="#051f38"
+              placeholder="Repeat password"
+              v-model="company.repeatpassword"
+              label="Repeat password"
+              outlined
+              append-icon="lock"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12">
+            <v-row>
+              <v-col cols="6" md="6">
+                <v-text-field
+                  dense
+                  large
+                  text
+                  color="#051f38"
+                  class="mt-n4"
+                  placeholder="Password"
+                  v-model="company.password"
+                  label="Password"
+                  outlined
+                  append-icon="lock"
+                  type="PASSWORD"
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="6" md="6">
+                <v-text-field
+                  dense
+                  large
+                  text
+                  color="#051f38"
+                  class="mt-n4"
+                  placeholder="Repeat Password"
+                  v-model="company.repeatpassword"
+                  label="Repeat Password"
+                  outlined
+                  append-icon="lock"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="088888888"
+              v-model="company.contactPhone"
+              label="Telefon"
+              outlined
+              append-icon="phone"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="088888888"
+              v-model="company.altPhone"
+              label="Alternativ Telefon"
+              outlined
+              append-icon="contact_phone"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="888888-8888"
+              v-model="company.organisationsnummer"
+              label="Organisationsnummer"
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="10" md="10">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="Gatunamn"
+              v-model="company.adress"
+              label="Adress"
+              outlined
+              append-icon="place"
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="2" md="2">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="Gatunamn"
+              v-model="company.houseNumber"
+              label="Adress"
+              outlined
+              append-icon="place"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="6" md="6	">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="19587"
+              v-model="company.postnummer"
+              label="Postnummer"
+              outlined
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="6" md="6	">
+            <v-text-field
+              dense
+              large
+              text
+              color="#051f38"
+              class="mt-n4"
+              placeholder="Postort"
+              v-model="company.city"
+              label="Ort"
+              outlined
+              append-icon="location_city"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row justify="end">
+          <v-col cols="12" md="3">
+            <v-btn @click="addCompanyUser" class="white--text btnColor">
+              Register
+            </v-btn>
+          </v-col>
+        </v-row>
+      </form>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      company: {
+        name: "",
+        password: "",
+        repeatPassword: "",
+        contactPerson: "",
+        contactEmail: "",
+        password: "",
+        repeatpassword: "",
+        contactPhone: "",
+        altPhone: "",
+        organisationsnummer: "",
+        adress: "",
+        postnummer: "",
+        city: "",
+      },
+    };
+  },
+  methods: {
+    async addCompanyUser() {
+      let companyRegInfo = {
+        companyname: this.company.name,
+        contactPerson: this.company.contactPerson,
+        contactEmail: this.company.contactEmail,
+        password: this.company.password,
+        phonenumber: this.company.contactPhone,
+        altPhone: this.company.altPhone,
+        streetname: this.company.adress,
+        city: this.company.city,
+        postcode: this.company.postnummer,
+        houseNumber: this.company.houseNumber,
+        organizationnumber: this.company.organisationsnummer,
+      };
+      console.log(companyRegInfo);
+      await this.$store.dispatch("company/registerCompanyUser", companyRegInfo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/ui/TestPerson.vue
@@ -0,0 +1,258 @@
+<template>
+  <v-row>
+    <v-col cols="12" md="12" sm="12">
+      <div class="container" md="6" sm="12">
+        <form class="mt-n12 pa-10">
+          <v-row d-flex justify="center">
+            <v-col cols="12" md="12">
+              <h3>Skappa Ett Privatkund Konto</h3>
+            </v-col>
+            <v-row d-flex justify="center">
+              <v-col cols="12" md="12">
+
+                <h3>{{ this.user.statusMessage }}</h3>
+              </v-col>
+            </v-row>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="12" sm="12">
+              <v-text-field
+                color="#051f38"
+                text
+                dense
+                class="mt-6 sizeLabel"
+                v-model="user.name"
+                append-icon="person"
+                label="Namn"
+                outlined
+                placeholder="Förnamn Efternamn"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col cols="12" md="12" sm="12">
+              <v-text-field
+                color="#051f38"
+                text
+                dense
+                class="mt-n3"
+                append-icon="email"
+                v-model="user.email"
+                label="Email"
+                outlined
+                placeholder="dinepst@mail.com"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="12">
+              <v-text-field
+                dense
+                large
+                text
+                color="#051f38"
+                class="mt-n4"
+                placeholder="Password"
+                v-model="user.password"
+                label="Password"
+
+                outlined
+                append-icon="lock"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="12" md="12">
+              <v-text-field
+                dense
+                large
+                text
+                color="#051f38"
+                class="mt-n4"
+                placeholder="repeat password"
+                v-model="user.repeatpassword"
+                label="Repeat Password"
+                outlined
+                append-icon="lock"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-col cols="12" md="12">
+              <v-text-field
+                dense
+                large
+                :type="password"
+                color="#051f38"
+                placeholder="Password"
+                v-model="user.password"
+                label="Password"
+                outlined
+                append-icon="lock"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-col cols="12" md="12">
+              <v-text-field
+                dense
+                large
+                :type="password"
+                color="#051f38"
+                placeholder="Repeat password"
+                v-model="user.repeatpassword"
+                label="Repeat password"
+                outlined
+                append-icon="lock"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field
+                dense
+                class="mt-n3"
+                color="#051f38"
+                append-icon="phone"
+                text
+                v-model="user.phone"
+                label="Phone"
+                outlined
+                placeholder="0855555555"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="6" sm="12">
+              <v-text-field
+                dense
+                class="mt-n3"
+                append-icon="contact_phone"
+                color="#051f38"
+                text
+                v-model="user.altPhone"
+                label="Alternativt Telefonnummer"
+                outlined
+                placeholder="0855555555"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-col cols="12" md="9" sm="6">
+              <v-text-field
+                dense
+                color="#051f38"
+                append-icon="place"
+                text
+                v-model="user.adress"
+                label="Street Name"
+                outlined
+                placeholder="odalvägen"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="3" sm="6">
+              <v-text-field
+                dense
+                color="#051f38"
+                text
+                v-model="user.houseNumber"
+                label="Number"
+                outlined
+                placeholder="23"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center">
+            <v-col cols="12" md="6" sm="6">
+              <v-text-field
+                dense
+                class="mt-n3"
+                append-icon="location_city"
+                color="#051f38"
+                text
+                v-model="user.city"
+                label="Postort"
+                outlined
+                placeholder="Linköping"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="6" sm="6">
+              <v-text-field
+                dense
+                class="mt-n3"
+                color="#051f38"
+                text
+                v-model="user.postnr"
+                label="Post nummer"
+                outlined
+                placeholder="19535"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row justify="end">
+            <v-col cols="12" md="3" sm="3">
+              <v-btn @click="addUser()" class="white--text btnColor"
+                >Register
+              </v-btn>
+            </v-col>
+          </v-row>
+        </form>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+//   props: {
+//     regInfo: Object,
+//   },
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        password: "",
+        repeatpassword: "",
+        phone: "",
+        altPhone: "",
+        adress: "",
+        city: "",
+        postnr: "",
+		houseNumber:""
+      },
+    };
+  },
+  methods: {
+    async addUser() {
+      let regInfo = {
+        name: this.user.name,
+        email: this.user.email,
+        password: this.user.password,
+        phonenumber: this.user.phone,
+        altPhone: this.user.altPhone,
+        streetname: this.user.adress,
+        city: this.user.city,
+        postcode: this.user.postnr,
+		houseNumber: this.user.houseNumber
+      };
+      console.log(regInfo);
+      await this.$store.dispatch("user/createNewUser", regInfo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-text-field >>> label {
+  font-size: 1.2em;
+  color: black;
+}
+</style>
