cfd8594c03c44d2e95721b235842eb8b6eeb4e3c
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-06-09 10:52:38 +0200

    readme file and some test specs for ensiklt dricks vatten

--- client/tests/unit/EnsikltDricksVatten.spec.js
+++ client/tests/unit/EnsikltDricksVatten.spec.js
@@ -23,6 +23,9 @@ describe('EnsikltDricksVatten.vue', () => {
             tests: [],
 
         }
+        mutations = {
+            increaseThecounterValue: jest.fn()
+        }
 
         store = new Vuex.Store({
             modules: {
@@ -72,28 +75,11 @@ describe('EnsikltDricksVatten.vue', () => {
 
         expect(title.text()).toBe(resultTitle)
     })
-    // it('should emit an event when the action v-btn is clicked', () => {
-    //     const wrapper = mount(CustomCard, {
-    //         localVue,
-    //         vuetify,
-    //         propsData: { title: 'Foobar' },
-    //     })
-
-    //     const event = jest.fn()
-    //     const button = wrapper.find('.v-btn')
-
-    //     // Here we bind a listener to the wrapper
-    //     // instance to catch our custom event
-    //     // https://vuejs.org/v2/api/#Instance-Methods-Events
-    //     wrapper.vm.$on('action-btn:clicked', event)
-
-    //     expect(event).toHaveBeenCalledTimes(0)
-
-    //     // Simulate a click on the button
-    //     button.trigger('click')
-
-    //     // Ensure that our mock event was called
-    //     expect(event).toHaveBeenCalledTimes(1)
-    // })
+    it('calls store action "moduleActionClick" when button is clicked', () => {
+        const wrapper = shallowMount(EnsikltDricksVatten, { store, localVue, vuetify })
+        const button = wrapper.find('v-btn.move-button')
+        button.trigger('click')
+        expect(mutations.increaseThecounterValue).toHaveBeenCalled()
+    })
 
 })
