58e4f87964c5a2cd43bbf430e0f860bc0cc8dac1
kaoskar1 <oscar.ljungdahl@hotmail.com>
2021-06-11 07:28:50 +0200

    final test oskar

--- client/tests/unit/kontaktsidan.spec.js
+++ client/tests/unit/kontaktsidan.spec.js
@@ -1,72 +1,62 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
+import { createLocalVue, mount, shallowMount, } from '@vue/test-utils'
 
-// Components
+import Vue from 'vue'
+// import Vuetify from 'vuetify'
+import vuetify from "vuetify"
+
 import KontaktSidan from '../../src/views/KontaktSidan'
 
-// Utilities
-import { createLocalVue, shallowMount, } from '@vue/test-utils'
-import Vuex from 'vuex'
-// import sinon from 'sinon'
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
+
+
+
+// const localVue = createLocalVue()
+
 
 
 describe('KontaktSidan.vue', () => {
-	let vuetify
-	let actions, state, store, mutations
+
+	let wrapper;
 
 	beforeEach(() => {
-		vuetify = new Vuetify()
-		state = {
+		const localVue = createLocalVue()
 
-		}
-		mutations = {
 
-		},
-
-			store = new Vuex.Store({
-				modules: {
-					tests: {
-						state,
-						actions,
-						mutations,
-						namespaced: true
-					}
-				}
-			})
-
+		Vue.use(vuetify);
+		wrapper = mount(KontaktSidan, { localVue })
 	})
-	test('is it a contact', async () => {
-		const wrapper = shallowMount(KontaktSidan)
+	test('is it a contact with h1', async () => {
+		// const wrapper = shallowMount(KontaktSidan)
 		const resText = 'Kontakta oss'
 		const h1Text = wrapper.find('h1')
 		expect(h1Text.text()).toBe(resText)
 	})
+	it('renders a vue instance', () => {
+		expect(shallowMount(KontaktSidan).isVueInstance()).toBe(true);
+	})
 
-	test('is it a contact', async () => {
-		const wrapper = shallowMount(KontaktSidan)
+	test('is it a p contact', async () => {
+		// const wrapper = shallowMount(KontaktSidan)
 		const resText = 'Skriv gärna ett mail till oss om du har några frågor eller problem'
 		const pText = wrapper.find('p')
 		expect(pText.text()).toBe(resText)
 	})
 
-
-	it('klickar på knappen', () => {
-		const wrapper = shallowMount(KontaktSidan,{propsData:{
-			
-		}}, {
-			localVue, vuetify, methods: {
-			postMessage: jest.fn()
-			}
-		})
-		const button = wrapper.find('v-btn')
-		button.trigger('click')
-
-
-		// expect(postMessage).toHaveBeenCalled()
+	it('kolla inskriven text', () => {
+		var email = wrapper.find('[data-test="mailText"]')
+		email.setValue('test text');
+		expect(wrapper.vm.email).toBe('test text')
 	})
+
+	it('klicka på save knappen o tom  input', () => {
+		wrapper.find('button').trigger("click");
+		var email = wrapper.find('[data-test="mailText"]')
+		expect(email.text()).toMatch('')
+		expect(wrapper.vm.email).toBe('')
+	})
+
+
+
 
 
 })
